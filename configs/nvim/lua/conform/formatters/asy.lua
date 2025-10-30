dc07a296df843ea4d89298ea223a47dedd0b42b4
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-10-30 12:33:56 +0100

    fix(nvim): make asy formatter repair more clang-format

--- configs/nvim/lua/conform/formatters/asy.lua
+++ configs/nvim/lua/conform/formatters/asy.lua
@@ -2,7 +2,7 @@ return {
 	format = function(self, ctx, lines, callback)
 		local out_lines = vim.deepcopy(lines)
 		for lnum, line in ipairs(out_lines) do
-			out_lines[lnum] = line:gsub("(%a)%-%- ", "%1--")
+			out_lines[lnum] = line:gsub("(%a)%-%- ", "%1--"):gsub(" ^ ^", "^^")
 		end
 		callback(nil, out_lines)
 	end,
