2c695c6b916b7acb036ff602e0329a39ef018cfb
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-11-09 17:25:09 +0100

    update

--- configs/nvim/lua/plugins/ui/indent.lua
+++ configs/nvim/lua/plugins/ui/indent.lua
@@ -18,4 +18,23 @@ return {
 			},
 		},
 	},
+	-- "saghen/blink.indent",
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- opts = {
+	-- 	static = {
+	-- 		enabled = true,
+	-- 		char = "│", -- ▎
+	-- 		priority = 1,
+	-- 		highlights = { "Indent" },
+	-- 	},
+	-- 	scope = {
+	-- 		enabled = true,
+	-- 		char = "│",
+	-- 		priority = 1000,
+	-- 		highlights = { "IndentScope" },
+	-- 		underline = {
+	-- 			enabled = false,
+	-- 		},
+	-- 	},
+	-- },
 }
