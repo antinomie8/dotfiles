a0ddcfb3def6d919e7c7d25b9ead43f6d5de0ec6
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-12-06 18:49:06 +0100

    feat(yazi): merge tab bar in header

--- configs/yazi/init.lua
+++ configs/yazi/init.lua
@@ -14,77 +14,148 @@ th.git.untracked_sign = " "
 th.git.ignored_sign = ""
 require("git"):setup()
 
-require("yatline"):setup({
-	section_separator = { open = "", close = "" },
-	-- part_separator = { open = "", close = "" },
-	part_separator = { open = "", close = "" },
-	inverse_separator = { open = "", close = "" },
+-- status
+function Status:mode()
+	local mode = tostring(self._tab.mode):upper()
 
-	style_a = {
-		fg = "#1f1f28",
-		bg_mode = {
-			normal = "#7e9cd8",
-			select = "#957FB8",
-			un_set = "#dca561",
-		},
-	},
+	local style = self:style()
+	return ui.Line({
+		ui.Span(th.status.sep_left.open):fg(style.main:bg()):bg("reset"),
+		ui.Span(" " .. mode .. " "):style(style.main),
+		ui.Span(th.status.sep_left.close):fg(style.main:bg()):bg(style.alt:bg()),
+	})
+end
 
-	style_b = { bg = "#54546d", fg = "#9cabca" },
-	style_c = { bg = "#2a2a37", fg = "#9cabca" },
+function Status:size()
+	-- local h = self._current.hovered
+	-- local size = h and (h:size() or h.cha.len) or 0
 
-	permissions_t_fg = "#98bb6c",
-	permissions_r_fg = "#e6c384",
-	permissions_w_fg = "#ff5d62",
-	permissions_x_fg = "#7aa89f",
-	permissions_s_fg = "#957fb8",
+	local style = self:style()
+	return ui.Line({
+		-- ui.Span(" " .. ya.readable_size(size) .. " "):style(style.alt),
+		ui.Span(th.status.sep_left.close):fg(style.alt:bg()),
+	})
+end
 
-	selected = { icon = "󰻭", fg = "#ffa066" },
-	copied = { icon = "", fg = "#98bb6c" },
-	cut = { icon = "", fg = "#e46876" },
-	files = { icon = "", fg = "#7e9cd8" },
-	filtereds = { icon = "", fg = "#957fb8" },
+function Status:name()
+	local h = self._current.hovered
+	if not h then
+		return ui.Line({})
+	end
+	local icon = h:icon()
 
-	total = { icon = "󰮍", fg = "#ff9e3b" },
-	succ = { icon = "", fg = "#7aa89f" },
-	fail = { icon = "", fg = "#e82424" },
-	found = { icon = "󰮕", fg = "#7e9cd8" },
-	processed = { icon = "󰐍", fg = "#6A9598" },
+	return ui.Line({
+		ui.Span(" " .. tostring(h.url) .. " "),
+		ui.Span(icon.text):style(icon.style),
+	})
+end
 
-	tab_width = 20,
-	tab_use_inverse = false,
+function Status:perm()
+	local h = self._current.hovered
+	if not h then
+		return ""
+	end
 
-	show_background = true,
+	local perm = h.cha:perm()
+	if not perm then
+		return ""
+	end
 
-	display_header_line = true,
-	display_status_line = true,
+	local spans = {}
+	for i = 1, #perm do
+		local c = perm:sub(i, i)
+		local style = th.status.perm_type
+		if c == "-" or c == "?" then
+			style = th.status.perm_sep
+		elseif c == "r" then
+			style = th.status.perm_read
+		elseif c == "w" then
+			style = th.status.perm_write
+		elseif c == "x" or c == "s" or c == "S" or c == "t" or c == "T" then
+			style = th.status.perm_exec
+		end
+		spans[i] = ui.Span(c):style(style)
+	end
+	return ui.Line(spans)
+end
 
-	component_positions = { "header", "tab", "status" },
+function Status:percent()
+	local percent = 0
+	local cursor = self._current.cursor
+	local length = #self._current.files
+	if cursor ~= 0 and length ~= 0 then
+		percent = math.floor((cursor + 1) * 100 / length)
+	end
 
-	status_line = {
-		left = {
-			section_a = {
-				{ type = "string", custom = false, name = "tab_mode" },
-			},
-			section_b = {
-				-- { type = "string", custom = false, name = "hovered_size" },
-			},
-			section_c = {
-				-- { type = "string", custom = false, name = "hovered_path" },
-				{ type = "coloreds", custom = false, name = "count" },
-			},
-		},
-		right = {
-			section_a = {
-				{ type = "string", custom = false, name = "date", params = { "%R" } },
-				{ type = "string", custom = true, name = "" },
-			},
-			section_b = {
-				{ type = "string", custom = false, name = "cursor_position" },
-				{ type = "string", custom = false, name = "cursor_percentage" },
-			},
-			section_c = {
-				{ type = "coloreds", custom = false, name = "permissions" },
-			},
-		},
-	},
-})
+	if percent == 0 then
+		percent = " Top "
+	elseif percent == 100 then
+		percent = " Bot "
+	else
+		percent = string.format(" %2d%% ", percent)
+	end
+
+	local style = self:style()
+	return ui.Line({
+		ui.Span(" " .. th.status.sep_right.open):fg(style.alt:bg()),
+		ui.Span(percent):style(style.alt),
+		ui.Span(string.format("%2d/%-2d ", math.min(cursor + 1, length), length)):style(style.alt),
+	})
+end
+
+function Status:position()
+	local style = self:style()
+	return ui.Line({
+		ui.Span(th.status.sep_right.open):fg(style.main:bg()):bg(style.alt:bg()),
+		ui.Span("  " .. tostring(os.date("%R")) .. " "):style(style.main),
+		ui.Span(th.status.sep_right.close):fg(style.main:bg()):bg("reset"),
+	})
+end
+
+-- header
+Header:children_add(function()
+	if #cx.tabs == 1 then return "" end
+	local lines = {}
+	if cx.tabs.idx ~= 1 then
+		lines[1] = ui.Line(th.tabs.sep_outer.open):fg(th.tabs.inactive:bg())
+	end
+
+	local max = 20
+	for i = 1, #cx.tabs do
+		if i == cx.tabs.idx then
+			local name = ui.truncate(string.format(
+				"%d %s",
+				i,
+				cx.tabs[i].name
+			), { max = max })
+			local left_sep = ui.Span(th.tabs.sep_inner.open):style(th.tabs.inactive)
+			local right_sep = ui.Span(th.tabs.sep_inner.close):style(th.tabs.inactive)
+			if i == 1 then
+				left_sep = left_sep:bg("reset")
+			end
+			if i == #cx.tabs then
+				right_sep = right_sep:bg("reset")
+			end
+			lines[#lines + 1] = ui.Line({
+				left_sep,
+				ui.Span(name):style(th.tabs.active),
+				right_sep,
+			})
+		else
+			local name = ui.truncate(string.format(
+				(i ~= 1 and " " or "") .. "%d %s" .. (i ~= #cx.tabs and " " or ""),
+				i,
+				cx.tabs[i].name
+			), { max = max })
+			lines[#lines + 1] = ui.Line(name):style(th.tabs.inactive)
+		end
+	end
+
+	if cx.tabs.idx ~= #cx.tabs then
+		lines[#lines + 1] = ui.Line(th.tabs.sep_outer.close):fg(th.tabs.inactive:bg())
+	end
+
+	return ui.Line(lines)
+end, 9000, Header.RIGHT)
+
+function Tabs.height() return 0 end
