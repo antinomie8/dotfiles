b87015f5ccd0069975e5459328029647bec3dbd7
anonymousgrasshopper <antoine.s.27182@gmail.com>
2026-01-08 12:09:22 +0100

    change asm tab width from 2 to 4

--- configs/nvim/after/ftplugin/asm.lua
+++ configs/nvim/after/ftplugin/asm.lua
@@ -1,3 +1,6 @@
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+
 -- assembles and links the current file
 vim.api.nvim_buf_create_user_command(0, "Assemble", function()
 	local filename = vim.fn.expand("%:r")
