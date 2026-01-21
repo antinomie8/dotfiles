9a784048d1209c5bbb177abbed96fc8c12ee3e57
Antoine Saez Dumas <antoine.saezdumas.git@gmail.com>
2026-01-21 20:15:47 +0100

    feat(nvim): improve ftplugin/cpp.lua

--- configs/nvim/after/ftplugin/cpp.lua
+++ configs/nvim/after/ftplugin/cpp.lua
@@ -1,25 +1,18 @@
 -- Switch to header files using gf
 vim.opt_local.suffixesadd:append({ ".h", ".hpp", ".cppm" })
-vim.opt_local.path:append({ "include" })
+vim.opt_local.path:append({ "include", vim.env.CPLUS_INCLUDE_PATH })
 
 -- debugging
 vim.b.codelldb_stdio_redirection = nil
-vim.keymap.set(
-	"n",
-	"<leader>oc",
-	function() vim.b.codelldb_stdio_redirection = not vim.b.codelldb_stdio_redirection end,
-	{ desc = "Toggle codelldb stdio redirection", buffer = true }
-)
+vim.keymap.set("n", "<leader>oc", function()
+	vim.b.codelldb_stdio_redirection = not vim.b.codelldb_stdio_redirection
+end, { desc = "Toggle codelldb stdio redirection", buffer = true })
 
+--debugger input
 local open_floating_window = function(filepath)
-	local buf = vim.api.nvim_get_current_buf()
-	local bufnr = vim.api.nvim_create_buf(true, false)
-	vim.api.nvim_buf_set_name(bufnr, filepath)
-
-	-- if the file exists, load its contents
-	if vim.fn.filereadable(filepath) == 1 then
-		vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.fn.readfile(filepath))
-	end
+	local main_buf = vim.api.nvim_get_current_buf()
+	local bufnr = vim.fn.bufadd(filepath)
+	vim.fn.bufload(bufnr)
 
 	-- set buffer options
 	vim.bo[bufnr].buftype = ""
@@ -29,9 +22,9 @@ local open_floating_window = function(filepath)
 	vim.keymap.set("n", "q", "<Cmd>wq<CR>", { buffer = bufnr, nowait = true, silent = true })
 
 	-- Dimensions
-	local width = math.floor(vim.o.columns * 0.8)
-	local height = math.floor(vim.o.lines * 0.8)
-	local row = math.floor((vim.o.lines - height) / 2)
+	local width = math.floor(vim.o.columns * 0.6)
+	local height = math.floor(vim.o.lines * 0.6)
+	local row = math.ceil((vim.o.lines - height) / 2)
 	local col = math.floor((vim.o.columns - width) / 2)
 
 	-- Create the floating window
@@ -52,8 +45,8 @@ local open_floating_window = function(filepath)
 	vim.api.nvim_create_autocmd("WinClosed", {
 		pattern = tostring(win_id),
 		callback = function()
-			vim.b[buf].stdio_completed = true
-			if vim.b[buf].compilation_completed then
+			vim.b[main_buf].stdio_completed = true
+			if vim.b[main_buf].compilation_completed then
 				vim.schedule(function() require("dap").continue() end)
 			end
 		end,
@@ -61,12 +54,9 @@ local open_floating_window = function(filepath)
 	})
 end
 local input_filename = vim.fn.expand("%:r") .. ".in"
-vim.keymap.set(
-	"n",
-	"<localleader>di",
-	function() open_floating_window(input_filename) end,
-	{ desc = "Edit debugger input ", buffer = true }
-)
+vim.keymap.set("n", "<localleader>di", function()
+	open_floating_window(input_filename)
+end, { desc = "Edit debugger input ", buffer = true })
 
 vim.keymap.set("n", "<localleader>dbg", function()
 	vim.cmd("silent! write")
@@ -82,12 +72,14 @@ vim.keymap.set("n", "<localleader>dbg", function()
 		"-o",
 		vim.fn.expand("%:r"),
 	}, {}, function(obj)
-		if obj.stderr then
-			if obj.stderr:match("error[^\n]*\n$") then
-				vim.notify(obj.stderr, vim.log.levels.ERROR, { title = "Compiler", icon = "" })
-			elseif obj.stderr ~= "" then
-				vim.notify(obj.stderr, vim.log.levels.WARN, { title = "Compiler", icon = "" })
-			end
+		if obj.stderr and obj.stderr ~= "" then
+			-- Fill quickfix list with errors/warnings
+			local lines = vim.split(obj.stderr, "\n")
+			vim.schedule(function()
+				vim.fn.setqflist({}, "r", { title = "Compiler " .. (obj.code == 0 and "Warnings" or "Errors"), lines = lines })
+				vim.cmd("copen")
+			end)
+			-- 	vim.notify(obj.stderr, (obj.code == 0) and vim.log.levels.WARN or vim.log.levels.ERROR, { title = "Compiler", icon = "" })
 		end
 		if obj.code == 0 then
 			vim.notify("Compilation completed", vim.log.levels.INFO, { title = "Debugging", icon = "" })
@@ -121,8 +113,8 @@ vim.keymap.set("n", "<localleader>dbg", function()
 end, { buffer = true })
 
 vim.keymap.set("n", "<localleader>rm", function()
-	for _, ext in ipairs({ "in", "out", "err" }) do
-		local filename = vim.fn.expand("%:r") .. "." .. ext
+	for _, ext in ipairs({ ".in", ".out", ".err", "" }) do
+		local filename = vim.fn.expand("%:r") .. ext
 		if vim.uv.fs_stat(filename) then
 			local ok, err = vim.uv.fs_unlink(filename)
 			if not ok then
@@ -137,9 +129,6 @@ vim.keymap.set("n", "<localleader>rm", function()
 end, { buffer = true })
 
 -- compile and run SFML programs
-vim.keymap.set(
-	"n",
-	"<localleader>sf",
-	function() vim.system({ "compile_sfml", vim.fn.expand("%:p:r") }, { text = true }) end,
-	{ expr = true, buffer = true }
-)
+vim.keymap.set("n", "<localleader>sf", function()
+	vim.system({ "compile_sfml", vim.fn.expand("%:p:r") }, { text = true })
+end, { expr = true, buffer = true })
