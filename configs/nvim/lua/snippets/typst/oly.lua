421b58c1518457342e79f918502a20a05c3fedea
Antoine Saez Dumas <antoine.saezdumas.git@gmail.com>
2026-06-06 15:59:30 +0200

    feat(nvim): refactor oly snippets

--- configs/nvim/lua/snippets/typst/oly.lua
+++ configs/nvim/lua/snippets/typst/oly.lua
@@ -1,200 +1,80 @@
 local ls = require("utils.snippets.luasnip")
-local s, i, d, fmt = ls.s, ls.i, ls.d, ls.fmt
+local s, t, d = ls.s, ls.t, ls.d
 local helpers = require("utils.snippets.helpers")
 local line_begin = helpers.line_begin
 local get_visual = helpers.get_visual
 
-return {
-	s({ trig = "!thm", dscr = "theorem", snippetType = "autosnippet", cond = line_begin },
-		fmt(
-			[[
-				#theorem[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!T", dscr = "theorem", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#theorem[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!lem", dscr = "lemma", snippetType = "autosnippet", cond = line_begin },
-		fmt(
-			[[
-				#lemma[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!L", dscr = "lemma", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#lemma[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!P", dscr = "proof", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#proof[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!cor", dscr = "corollary", snippetType = "autosnippet", cond = line_begin },
-		fmt(
-			[[
-				#corollary[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!C", dscr = "corollary", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#corollary[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!prop", dscr = "proposition", snippetType = "autosnippet", cond = line_begin },
-		fmt(
-			[[
-				#proposition[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!exer", dscr = "exercise", snippetType = "autosnippet", cond = line_begin },
-		fmt(
-			[[
-				#exercise[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!E", dscr = "exercise", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#exercise[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!R", dscr = "remark", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#remark[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!D", dscr = "definition", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#definition[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!N", dscr = "notation", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#notation[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!S", dscr = "solution", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#solution[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s({ trig = "!A", dscr = "algorithm", snippetType = "autosnippet", cond = line_begin, hidden = true },
-		fmt(
-			[[
-				#algorithm[
-					<>
-				]
-			]],
-			{
-				i(1),
-			}
-		)
-	),
-	s(
+local snippets = {
+	s({ trig = "voc", dscr = "vocabulary" },
 		{
-			trig = "voc",
-			dscr = "vocabulary",
-		},
-		fmt(
-			'#vocab("<>")',
-			{
-				d(1, get_visual),
-			}
-		)
+			t('#vocab("'),
+			d(1, get_visual),
+			t('")'),
+		}
+	),
+	s({ trig = "!V", dscr = "vocabulary", snippetType = "autosnippet" },
+		{
+			t('#vocab("'),
+			d(1, get_visual),
+			t('")'),
+		}
 	),
 }
+
+local envs = {
+	"theorem",
+	"lemma",
+	"proof",
+	"corollary",
+	"exercise",
+	"remark",
+	"definition",
+	"notation",
+	"solution",
+	"algorithm",
+}
+
+local abbrs = {
+	["theorem"] = "thm",
+	["lemma"] = "lem",
+	["corollary"] = "cor",
+	["proposition"] = "prop",
+	["exercise"] = "exer",
+	["definition"] = "def",
+	["solution"] = "sol",
+	["algorithm"] = "algo",
+}
+
+for _, env in ipairs(envs) do
+	table.insert(snippets, s(
+		{
+			trig = "!" .. env:sub(1, 1):upper(),
+			dscr = env,
+			snippetType = "autosnippet",
+			cond = line_begin,
+			hidden = true,
+		},
+		{
+			t({ "#" .. env .. "[", "\t" }),
+			d(1, get_visual),
+			t({ "", "]" }),
+		}
+	))
+end
+for env, abbr in ipairs(abbrs) do
+	table.insert(snippets, s(
+		{
+			trig = "!" .. abbr,
+			dscr = env,
+			snippetType = "autosnippet",
+			cond = line_begin,
+		},
+		{
+			t({ "#" .. env .. "[", "", "\t" }),
+			d(1, get_visual),
+			t({ "", "]" }),
+		}
+	))
+end
+
+return snippets
